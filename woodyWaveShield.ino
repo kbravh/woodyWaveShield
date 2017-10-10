1b0080d7a9b5539048f7859252fb06b823360fa5
Karey Higuera <30562119+operaartificis@users.noreply.github.com>
2017-10-09 22:03:39 -0500

    Add libraries and playWav function
    Added calls to necessary wave libraries, and added function that plays the called wav file until it is finished.

--- woodyWaveShield.ino
+++ woodyWaveShield.ino
@@ -1,1 +1,42 @@
-/* * Woody's pullstring voice box. Each time the Arduino is triggered,  * presumably by the pullstring on Woody's back, a random catchphrase  * will be selected and played. */void setup() {    }void loop() {    }
+/*
+ * Woody's pullstring voice box. Each time the Arduino is triggered, 
+ * presumably by the pullstring on Woody's back, a random catchphrase 
+ * will be selected and played.
+ */
+
+#include <WaveHC.h>
+#include <WaveUtil.h>
+
+void setup() {
+    
+}
+
+void loop() {
+    
+}
+
+void playWav(char *name) {
+  //stop a wave file if it's already playing
+  if (wave.isplaying) {
+    wave.stop(); 
+  }
+  //error if file cannot be opened
+  if (!file.open(root, name)) {
+    Serial.print(F("Couldn't open file "));
+    Serial.print(name); 
+    return;
+  }
+  //error if WAV file is invalid
+  if (!wave.create(file)) {
+    Serial.println(F("Not a valid WAV"));
+    return;
+  }
+  //play sound file if no error is found
+  wave.play();
+  
+  //plays file until it finishes without interruptions
+  while (wave.isplaying);
+  
+  // see if an error occurred while playing
+  sdErrorCheck();
+}

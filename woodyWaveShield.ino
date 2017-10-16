6480352df66b48353f50276eb06e2d8c7dc4ec24
Karey Higuera <30562119+opusartificis@users.noreply.github.com>
2017-10-16 07:58:37 -0500

    Update woodyWaveShield.ino

--- woodyWaveShield.ino
+++ woodyWaveShield.ino
@@ -34,7 +34,7 @@ void loop() {
      * possibly implement code from openByIndex?
      */
     if(switchState==1){
-        char phrase = random(0,6);
+        char phrase = random(0,6); //0-5, Random() is non-inclusive
         playWav(phrase);
     }
 }
